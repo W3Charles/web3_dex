00fec60b46c24c51404652f58e8314399cd8a041
W3Charles <www3charles@gmail.com>
2023-04-28 05:09:08 -0400

    feat: connect front end to 1inch Aggregation API

--- dex/src/components/Swap.js
+++ dex/src/components/Swap.js
@@ -7,8 +7,11 @@ import {
 } from "@ant-design/icons";
 import tokenList from "../tokenList.json";
 import axios from "axios";
+import { useSendTransaction, useWaitForTransaction } from "wagmi";
 
-function Swap() {
+
+function Swap(props) {
+  const {address, isConnected} = props;
   const [slippage, setSlippage] = useState(2.5);
   const [tokenOneAmount, setTokenOneAmount] = useState(null);
   const [tokenTwoAmount, setTokenTwoAmount] = useState(null);
@@ -17,7 +20,20 @@ function Swap() {
   const [isOpen, setIsOpen] = useState(false);
   const [changeToken, setChangeToken] = useState(1);
   const [prices, setPrices] = useState(null);
+  const [txDetails, setTxDetails] = useState({
+    to: null,
+    data: null,
+    value: null,
+  });
 
+  const {data, sendTransaction} = useSendTransaction({
+    request: {
+      from: address,
+      to: String(txDetails.to),
+      data: String(txDetails.data),
+      value: String(txDetails.value),
+    }
+  })
 
   function handleSlippageChange(e){
     setSlippage(e.target.value);
@@ -66,13 +82,44 @@ function Swap() {
     const res = await axios.get(`http://localhost:3001/tokenPrice`, {
       params: {addressOne: one, addressTwo: two}
     })
-    console.log(res.data);
+    
     setPrices(res.data)
+  }
+
+  async function fetchDexSwap(){
+
+    const allowance = await axios.get(`https://api.1inch.io/v5.0/1/approve/allowance?tokenAddress=${tokenOne.address}&walletAddress=${address}`)
+
+    if (allowance.data.allowance === "0"){
+      const approve = await axios.get(`https://api.1inch.io/v5.0/1/approve/transaction?tokenAddress=${tokenOne.address}`)
+
+      setTxDetails(approve.data);
+      console.log("not approved")
+      return
+    }
+
+    const tx = await axios.get(
+      //Convert to whole numbers. Can only swap whole numbers
+      `https://api.1inch.io/v5.0/1/swap?fromTokenAddress=${tokenOne.address}&toTokenAddress=${tokenTwo.address}&amount=${tokenOneAmount.padEnd(tokenOne.decimals+tokenOneAmount.length, '0')}&fromAddress=${address}&slippage=${slippage}`
+    )
+
+    let decimals = Number(`1E${tokenTwo.decimals}`)
+    setTokenTwoAmount((Number(tx.data.toTokenAmount)/decimals).toFixed(2));
+
+    setTxDetails(tx.data.tx);
+
   }
 
   useEffect(()=>{
     fetchPrices(tokenList[0].address, tokenList[1].address)
   }, [])
+
+  useEffect(()=>{
+    if(txDetails.to && isConnected){
+      sendTransaction();
+    }
+  }, [txDetails])
+
 
   const settings = (
     <>
@@ -146,7 +193,7 @@ function Swap() {
           <DownOutlined />
         </div>
       </div>
-      <div className="swapButton" disabled={!tokenOneAmount}>Swap</div>
+      <div className="swapButton" disabled={!tokenOneAmount || !isConnected} onClick={fetchDexSwap}> Swap </div>
       </div>
     </>
   )
